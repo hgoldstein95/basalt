d5d3cfb71fd0e4246a5ba6d5e5f8323daa48f540
Ernest Ng <eyn5@cornell.edu>
2025-12-19 10:51:42 -0500

    Rename Term to Expr

--- Basalt/Examples/STLC.lean
+++ Basalt/Examples/STLC.lean
@@ -1,4 +1,5 @@
 import Basalt
+import Basalt.Examples.NatList
 
 open RandomChoice
 
@@ -24,28 +25,31 @@ def typeToString (ty : Ty) : String :=
 instance : Repr Ty where
   reprPrec ty _ := typeToString ty
 
-/-- STLC terms using De Bruijn indices,
-    extended with naturals and addition -/
-inductive Term where
-  | Const: Nat → Term
-  | Add: Term → Term → Term
-  | Var: Nat → Term
-  | App: Term → Term → Term
-  | Abs: Ty → Term → Term
+/-- A typing context `Ctx` is just a list of types -/
+abbrev Ctx := List Ty
+
+/-- STLC expressions using De Bruijn indices,
+    extended with Nats & addition -/
+inductive Expr where
+  | Const: Nat → Expr
+  | Add: Expr → Expr → Expr
+  | Var: Nat → Expr
+  | App: Expr → Expr → Expr
+  | Abs: Ty → Expr → Expr
   deriving BEq
 
-/-- Pretty-printer for `Term`s -/
-def termToString (e : Term) : String :=
+/-- Pretty-printer for `Expr`s -/
+def exprToString (e : Expr) : String :=
   match e with
   | .Const n => s!"Const {n}"
-  | .Add e1 e2 => s!"({termToString e1} + {termToString e2})"
+  | .Add e1 e2 => s!"({exprToString e1} + {exprToString e2})"
   | .Var id => s!"Id {id}"
-  | .App e1 e2 => s!"({termToString e1} {termToString e2})"
-  | .Abs τ e2 => s!"(λ _ : {typeToString τ}. {termToString e2})"
+  | .App e1 e2 => s!"({exprToString e1} {exprToString e2})"
+  | .Abs τ e2 => s!"(λ _ : {typeToString τ}. {exprToString e2})"
 
-/-- Repr instance for `term`s -/
-instance : Repr Term where
-  reprPrec (e : Term) _ := termToString e
+/-- Repr instance for `Expr`s -/
+instance : Repr Expr where
+  reprPrec (e : Expr) _ := exprToString e
 
 ----------------------
 -- Generators
@@ -61,7 +65,20 @@ def Ty.genTy : Gen Ty :=
        return .Fun t1 t2)
 partial_fixpoint
 
+-- We open the `NatList` namesapce just so we can use `Nat.arbitrary` below
+open NatListExample
 
+-- TODO:
+-- * add sub-generators for Abs, Add, Var
+-- * parameterize the generator by Ctx
+def Expr.genExpr : Gen Expr :=
+  pick
+    (fun () => .Const <$> Nat.arbitrary)
+    (fun () => do
+       let e1 ← Expr.genExpr
+       let e2 ← Expr.genExpr
+       return .App e1 e2)
+partial_fixpoint
 
 
 end STLCExample
