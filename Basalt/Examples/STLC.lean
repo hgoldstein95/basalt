500e2b640273bc3f89c1937e5786b9499e978a1d
Ernest Ng <eyn5@cornell.edu>
2025-12-18 19:21:34 -0500

    Rename STLC types + generator for STLC types

--- Basalt/Examples/STLC.lean
+++ Basalt/Examples/STLC.lean
@@ -4,34 +4,38 @@ open RandomChoice
 
 namespace STLCExample
 
+----------------------
+-- Type Definitions
+----------------------
+
 /-- Base types in the STLC (either Nat or functions) -/
-inductive type where
-  | Nat : type
-  | Fun: type → type → type
+inductive Ty where
+  | Nat : Ty
+  | Fun: Ty → Ty → Ty
   deriving BEq, DecidableEq
 
-/-- Pretty-printer for `type`s -/
-def typeToString (ty : type) : String :=
+/-- Pretty-printer for `Ty`s -/
+def typeToString (ty : Ty) : String :=
   match ty with
   | .Nat => "ℕ"
   | .Fun τ1 τ2 => s!"{typeToString τ1} → {typeToString τ2}"
 
 /-- Repr instance for `type`s -/
-instance : Repr type where
+instance : Repr Ty where
   reprPrec ty _ := typeToString ty
 
 /-- STLC terms using De Bruijn indices,
     extended with naturals and addition -/
-inductive term where
-  | Const: Nat → term
-  | Add: term → term → term
-  | Var: Nat → term
-  | App: term → term → term
-  | Abs: type → term → term
+inductive Term where
+  | Const: Nat → Term
+  | Add: Term → Term → Term
+  | Var: Nat → Term
+  | App: Term → Term → Term
+  | Abs: Ty → Term → Term
   deriving BEq
 
-/-- Pretty-printer for `term`s -/
-def termToString (e : term) : String :=
+/-- Pretty-printer for `Term`s -/
+def termToString (e : Term) : String :=
   match e with
   | .Const n => s!"Const {n}"
   | .Add e1 e2 => s!"({termToString e1} + {termToString e2})"
@@ -40,7 +44,24 @@ def termToString (e : term) : String :=
   | .Abs τ e2 => s!"(λ _ : {typeToString τ}. {termToString e2})"
 
 /-- Repr instance for `term`s -/
-instance : Repr term where
-  reprPrec (e : term) _ := termToString e
+instance : Repr Term where
+  reprPrec (e : Term) _ := termToString e
+
+----------------------
+-- Generators
+----------------------
+
+/-- A naive generator for STLC types -/
+def Ty.genTy : Gen Ty :=
+  pick
+    (fun () => pure .Nat)
+    (fun () => do
+       let t1 ← genTy
+       let t2 ← genTy
+       return .Fun t1 t2)
+partial_fixpoint
+
+
+
 
 end STLCExample
