031b9748ee623f5ac5831b683c42ad633fcd5cf2
Ernest Ng <eyn5@cornell.edu>
2025-12-28 12:13:44 +0800

    Add oneOf combinator

--- Basalt/RandomChoice.lean
+++ Basalt/RandomChoice.lean
@@ -4,12 +4,23 @@ class RandomChoice (m : Type → Type) where
   choose : (lo hi : Nat) → (h : lo ≤ hi) → m Nat
 
 /-- Picks between two (thunked) generators, each with probability 0.5 -/
-def RandomChoice.pick [Monad m] [RandomChoice m] (x y : Unit → m α) := do
+def RandomChoice.pick [Monad m] [RandomChoice m] (x y : Unit → m α) : m α := do
   if (← choose 0 1 (by simp)) == 0 then x () else y ()
 
 /-- Biased coin-flip with success probability `r` -/
 def RandomChoice.coin [Monad m] [RandomChoice m] (r : Rat) : m Bool := do
   if (← choose 0 r.den (by simp)) < r.num then pure true else pure false
+
+/-- Picks a generator from a list of generators -/
+def RandomChoice.oneOf
+  {m : Type → Type}
+  {α : Type}
+  [Monad m]
+  [RandomChoice m]
+  [Inhabited (m α)]
+  (gs : List (Unit → m α)) : m α := do
+  let idx ← RandomChoice.choose 0 gs.length (by simp)
+  gs[idx]! ()
 
 open Lean.Order in
 @[partial_fixpoint_monotone]
