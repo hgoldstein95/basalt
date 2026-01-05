ebff28578c4e70ac02cb10abb022a371928294df
Ernest Ng <eyn5@cornell.edu>
2026-01-05 22:39:05 +0800

    Fix indentation

--- Basalt/Experiments/BDT/STLC/Generator.lean
+++ Basalt/Experiments/BDT/STLC/Generator.lean
@@ -89,76 +89,76 @@ def genVar (vars : List Nat) : Gen Expr := do
   let idx ← choose 0 (vars.length - 1) (by omega)
   return .Var vars[idx]!
 
-  /-- BDT STLC generator of well-typed expressions `e` where `Γ ⊢ e : τ`.
-    Constructor arities:
-    - Arity 0: Bool, Var (weight t0)
-    - Arity 1: Abs (weight t1 * α)
-    - Arity 2: App (weight t2 * α * α)
-  -/
-  def Expr.genExprWithBDT (α t0 t1 t2 d : Rat) (Γ : Ctx) (τ : Ty): Gen Expr := do
-    let leafWeight := t0          -- "Leaves" are Bool & Var (0 recursive calls to `Expr`)
-    let absWeight := t1 * α       -- Abs has 1 recursive call
-    let appWeight := t2 * α * α   -- App has 2 recursive calls
+/-- BDT STLC generator of well-typed expressions `e` where `Γ ⊢ e : τ`.
+  Constructor arities:
+  - Arity 0: Bool, Var (weight t0)
+  - Arity 1: Abs (weight t1 * α)
+  - Arity 2: App (weight t2 * α * α)
+-/
+def Expr.genExprWithBDT (α t0 t1 t2 d : Rat) (Γ : Ctx) (τ : Ty): Gen Expr := do
+  let leafWeight := t0          -- "Leaves" are Bool & Var (0 recursive calls to `Expr`)
+  let absWeight := t1 * α       -- Abs has 1 recursive call
+  let appWeight := t2 * α * α   -- App has 2 recursive calls
 
-    let totalWeight := leafWeight + absWeight + appWeight
+  let totalWeight := leafWeight + absWeight + appWeight
 
-    let leafChoice ← coin (leafWeight / totalWeight)
-    if leafChoice then
-      -- Generate a leaf
-      match τ with
-      | .TBool =>
-        -- Try to generate a variable if context has one of this type
-        let vars := List.filter (fun i => Γ[i]! == τ) (List.range Γ.length)
+  let leafChoice ← coin (leafWeight / totalWeight)
+  if leafChoice then
+    -- Generate a leaf
+    match τ with
+    | .TBool =>
+      -- Try to generate a variable if context has one of this type
+      let vars := List.filter (fun i => Γ[i]! == τ) (List.range Γ.length)
 
-        -- TODO: is this right? I wonder if we should just have different
-        -- weights for `Var` & `Bool` instead of conflating `t0` for
-        -- both of them (i.e. perform a weighted choice here instead of `pick`)
-        if !vars.isEmpty then
-          pick
-            (fun () => genVar vars)
-            (fun () => genBoolExpr)
-        else
-          genBoolExpr
-      | .TFun τ1 τ2 =>
-        -- For function types, see if there is already a variable of type
-        -- `τ1 → τ2` in the context. If yes, pick between generating that
-        -- variable and generating a new Abs.
-        let vars := List.filter (fun i => Γ[i]! == τ) (List.range Γ.length)
-        if !vars.isEmpty then
-          pick
-            (fun () => genVar vars)
-            (fun () => genAbs τ1 τ2)
-        else
-          genAbs τ1 τ2
-    else
-      -- Choose between Abs and App
-      let absChoice ← coin (absWeight / (absWeight + appWeight))
-
-      if absChoice then
-        -- We can't generate an abstraction of type `TBool`,
-        -- so we just generate a Bool when `τ = Tbool`
-        match τ with
-        | .TBool => genBoolExpr
-        | .TFun τ1 τ2 => genAbs τ1 τ2
+      -- TODO: is this right? I wonder if we should just have different
+      -- weights for `Var` & `Bool` instead of conflating `t0` for
+      -- both of them (i.e. perform a weighted choice here instead of `pick`)
+      if !vars.isEmpty then
+        pick
+          (fun () => genVar vars)
+          (fun () => genBoolExpr)
       else
-        genApp
-    partial_fixpoint
-  where
-    -- Generates an `Abs` of type `τ1 → τ2`
-    genAbs (τ1 τ2 : Ty) : Gen Expr := do
-      let α' := α * d
-      let body ← genExprWithBDT α' t0 t1 t2 d (τ1 :: Γ) τ2
-      return .Abs τ1 body
-    partial_fixpoint
+        genBoolExpr
+    | .TFun τ1 τ2 =>
+      -- For function types, see if there is already a variable of type
+      -- `τ1 → τ2` in the context. If yes, pick between generating that
+      -- variable and generating a new Abs.
+      let vars := List.filter (fun i => Γ[i]! == τ) (List.range Γ.length)
+      if !vars.isEmpty then
+        pick
+          (fun () => genVar vars)
+          (fun () => genAbs τ1 τ2)
+      else
+        genAbs τ1 τ2
+  else
+    -- Choose between Abs and App
+    let absChoice ← coin (absWeight / (absWeight + appWeight))
 
-    -- Generates an application
-    genApp : Gen Expr := do
-      let α' := α * d
-      let argTy ← Ty.genTyWithBDT α' t0 t2 d
-      let e1 ← genExprWithBDT α' t0 t1 t2 d Γ (.TFun argTy τ)
-      let e2 ← genExprWithBDT α' t0 t1 t2 d Γ argTy
-      return .App e1 e2
-    partial_fixpoint
+    if absChoice then
+      -- We can't generate an abstraction of type `TBool`,
+      -- so we just generate a Bool when `τ = Tbool`
+      match τ with
+      | .TBool => genBoolExpr
+      | .TFun τ1 τ2 => genAbs τ1 τ2
+    else
+      genApp
+  partial_fixpoint
+where
+  -- Generates an `Abs` of type `τ1 → τ2`
+  genAbs (τ1 τ2 : Ty) : Gen Expr := do
+    let α' := α * d
+    let body ← genExprWithBDT α' t0 t1 t2 d (τ1 :: Γ) τ2
+    return .Abs τ1 body
+  partial_fixpoint
+
+  -- Generates an application
+  genApp : Gen Expr := do
+    let α' := α * d
+    let argTy ← Ty.genTyWithBDT α' t0 t2 d
+    let e1 ← genExprWithBDT α' t0 t1 t2 d Γ (.TFun argTy τ)
+    let e2 ← genExprWithBDT α' t0 t1 t2 d Γ argTy
+    return .App e1 e2
+  partial_fixpoint
 
 
 /-- Generates a well-typed expression `e` such that `Γ ⊢ e : τ`
