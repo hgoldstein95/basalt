814eb62e1b007d0cc23838861bde08f065e8b7b5
Ernest Ng <eyn5@cornell.edu>
2026-01-05 16:07:15 +0800

    Add generator for BDTParams

--- Basalt/Experiments/BDT/Hyperparameters.lean
+++ Basalt/Experiments/BDT/Hyperparameters.lean
@@ -1,5 +1,6 @@
 import Basalt
 
+open RandomChoice
 
 /-- Hyperparameters for Boltzmann Decay Tuning -/
 structure BDTParams where
@@ -27,3 +28,26 @@ def BDTParams.BSTdefault : BDTParams :=
   , t1 := 0.0
   , t2 := 1.0
   , decay := 0.5 }
+
+/-- Generates a rational number within
+    in the unit interval `[0, 1]` (inclusive )-/
+def genUnitIntervalRat (denominator : Nat) : Gen Rat := do
+  let numerator ← choose 0 denominator (by simp)
+  return (numerator / denominator)
+
+/-- Generates a set of BDT parameters.
+    (Note: this is currently unused,
+    the idea being we can tune this generator later and sample lots
+    of different parameters for an ensemble approach to generation)  -/
+def genBDTParams : Gen BDTParams := do
+  let decay ← genUnitIntervalRat 10
+  let t0 ← genUnitIntervalRat 10
+  let t1 ← genUnitIntervalRat 10
+  let t2 ← genUnitIntervalRat 10
+
+  -- To generate a value for α_0, we first generate a base value
+  -- then multiply it by some factor between 1 & 5.
+  -- This allows us to generate values for α_0 ∈ [0, 5]
+  let alpha0_base ← genUnitIntervalRat 10
+  let multiplier ← choose 1 5 (by omega)
+  return { alpha0 := alpha0_base * multiplier, t0, t1, t2, decay }
