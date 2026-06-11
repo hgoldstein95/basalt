595e6f707eff112e2392a8823450ef5aadb1dec2
Ernest Ng <eyn5@cornell.edu>
2026-06-11 13:25:03 -0400

    Char & String generators + SPMF Proofs (#4)
    * Add basic generator for alphanumeric chars + proofs
    
    * Add comments to ArbChar
    
    * Document ArbChar lemmas
    
    * Begin String.arbitrary
    
    * Prove that String.arbitrary is an SPMF
    
    * Rewrite ArbString support proof to be more readable
    
    * update Char.arbitrary_cost
    
    * genCharList_cost
    
    * stuck on String.arbitrary_cost
    
    * Finish String.arbitrary_cost
    
    * Use a tighter cost function
    
    * Make the proof of Char.arbitrary_support a bit more readable

---
+++ Basalt/Examples/ArbChar.lean
@@ -0,0 +1,76 @@
+import Basalt
+import Batteries.Data.Char
+
+open RandomChoice
+
+namespace ArbChar
+
+-- The space of Unicode values that correspond to alphanumeric chars
+-- consist of 3 disjoint intervals (48–57, 65–90, 97–122),
+-- so we can't just generate an arbitrary `Nat` and then pass it to `Char.ofNat`.
+-- Instead, we define this helper function, which we call in `Char.arbitrary` below
+-- so that we can just generate a `Nat` in the interval [0, 61], which simplifes
+-- the proofs below.
+private def indexToChar (n : Nat) : Char :=
+  if n < 10 then Char.ofNat (n + 48)
+  else if n < 36 then Char.ofNat (n - 10 + 65)
+  else Char.ofNat (n - 36 + 97)
+
+/-- Generates a random alphanumeric character -/
+def Char.arbitrary [Gen G] : G Char := do
+  let n ← choose 0 61 (by omega)
+  pure (indexToChar n.down)
+
+private theorem indexToChar_isAlphanum :
+    ∀ n, n ≤ 61 → (indexToChar n).isAlphanum = true := by decide
+
+-- `decide` is too inefficient here, so we need to use `native_decide`
+private theorem isAlphanum_exists_index :
+    ∀ c : Char, c.isAlphanum = true → ∃ n, n ≤ 61 ∧ indexToChar n = c := by native_decide
+
+/-- All alphanumeric characters are generable by `Char.arbitrary` -/
+theorem Char.arbitrary_support :
+    c ∈ SPMF.support Char.arbitrary ↔ c.isAlphanum = true := by
+  constructor
+  · -- c ∈ SPMF.support Char.arbitrary -> c.isAlphanum = true
+    intro h
+    rw [Char.arbitrary] at h
+    simp at h
+    obtain ⟨n, hle, rfl⟩ := h
+    apply indexToChar_isAlphanum
+    assumption
+  · -- c.isAlphanum = true -> c ∈ SPMF.support Char.arbitrary
+    intro h
+    rw [Char.arbitrary]
+    simp
+    obtain ⟨n, hhi, hc⟩ := isAlphanum_exists_index c h
+    exists n
+    constructor
+    . assumption
+    . apply hc.symm
+
+/-- `Char.arbitrary` almost surely terminates -/
+theorem Char.arbitrary_terminates : SPMF.IsPMF Char.arbitrary := by
+  unfold Char.arbitrary
+  apply SPMF.IsPMF_bind_pure
+  apply SPMF.IsPMF_choose
+
+-- Proof is similar to `Nat.arbitrary_cost`, but `Char.arbitrary` makes only
+-- one random choice, so `fun _ => 1` suffices as the cost function
+theorem Char.arbitrary_cost :
+    IsBounded Char.arbitrary (fun _ => 1) := by
+  rw [IsBounded_iff]
+  intro ⟨c, cost⟩ h
+  rw [Char.arbitrary] at h
+  simp [SPMF.Cost.mem_support_bind_iff, SPMF.Cost.mem_support_choose_iff,
+          SPMF.Cost.mem_support_pure_iff] at h
+  obtain ⟨ n, ⟨ _, heq ⟩, _ ⟩ := h
+  subst heq
+  dsimp
+  constructor
+
+#guard_msgs(drop info) in
+#eval (for _ in [0:20] do
+  IO.println <| repr (← Char.arbitrary) : IO Unit)
+
+end ArbChar
