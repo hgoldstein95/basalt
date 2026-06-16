194a04dd7e32e95ab92c90076773f15844959ddf
Harrison Goldstein <hgoldstein95@gmail.com>
2026-06-15 23:14:28 -0400

    Remove exe, not really useful

--- Main.lean
+++
@@ -1,9 +0,0 @@
-/-
-Copyright (c) 2026 Harrison Goldstein. All rights reserved.
-Released under MIT license as described in the file LICENSE.
-Authors: Harrison Goldstein
--/
-import Basalt
-import Basalt.Examples.Basic
-
-def main : IO Unit := pure ()
