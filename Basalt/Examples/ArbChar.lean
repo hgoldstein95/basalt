98f919b6f9a5f4e3ac940b22ee33a78434ce4230
Ernest Ng <eyn5@cornell.edu>
2026-06-24 11:13:36 -0400

    Fix Char.arbitrary_cost proof

--- Basalt/Examples/ArbChar.lean
+++ Basalt/Examples/ArbChar.lean
@@ -20,6 +20,8 @@ theorem Char.arbitrary_terminates : SPMF.IsPMF Char.arbitrary := by
   unfold Char.arbitrary
   apply SPMF.IsPMF_elements
 
+-- Proof is similar to `Nat.arbitrary_cost`, but `Char.arbitrary` makes only
+-- one random choice, so `fun _ => 1` suffices as the cost function
 theorem Char.arbitrary_cost :
     IsBounded Char.arbitrary (fun _ => 1) := by
   rw [IsBounded_iff]
@@ -27,19 +29,19 @@ theorem Char.arbitrary_cost :
   rw [Char.arbitrary, elements] at h
   simp only [SPMF.Cost.mem_support_bind_iff, Functor.map] at h
   obtain ⟨⟨i, hi⟩, n1, n2, h1, h2, rfl⟩ := h
-  -- h2: (c, n2) ∈ support of (pure alphanumChars[i]) in Cost monad → n2 = 0
-  have hlen : alphanumChars.length = 62 := by native_decide
+  have hlen : alphanumChars.length = 62 := by decide
   replace h2 : n2 = 0 := by
     split at h2
     simp only [Pure.pure, SPMF.mem_support_pure_iff, Prod.mk.injEq] at h2
-    exact h2.2
+    have ⟨_, h⟩ := h2
+    assumption
   subst h2
-  -- h1: (⟨i,hi⟩, n1) ∈ support of (ULift.down <$> choose) in Cost monad → n1 = 1
   replace h1 : n1 = 1 := by
     revert h1
     simp [SPMF.mem_support_bind_iff, SPMF.mem_support_pure_iff]
   subst h1
-  simp
+  dsimp
+  constructor
 
 #guard_msgs(drop info) in
 #eval (for _ in [0:20] do
