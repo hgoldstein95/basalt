7b4a9d6efea8d3dea6694597b39fe20173741698
Ernest Ng <eyn5@cornell.edu>
2026-07-07 16:14:30 -0400

    Add support_vectorOf lemma

--- Basalt/SPMF/Support.lean
+++ Basalt/SPMF/Support.lean
@@ -153,6 +153,73 @@ theorem mem_support_pick_iff
     a ∈ (pick (fun () => x) (fun () => y)).support ↔ a ∈ x.support ∨ a ∈ y.support := by
   simp
 
+/-- The support of `vectorOf n g` is the set of all length-`n` list where
+    each element is in `g`'s support -/
+theorem support_vectorOf
+    {n : Nat}
+    {g : SPMF α} :
+    support (vectorOf n g) = { xs | List.length xs = n ∧ ∀ x ∈ xs, x ∈ g.support } := by
+  induction n with
+  | zero =>
+    simp [vectorOf]
+    ext a
+    dsimp only [Set.mem_setOf_eq]
+    constructor
+    . intro h
+      rw [Set.mem_singleton_iff] at h
+      constructor
+      . assumption
+      . intro x hmem
+        subst h
+        contradiction
+    . intro h
+      obtain ⟨heq, hmem⟩ := h
+      apply Set.mem_singleton_iff.mpr
+      assumption
+  | succ n' IH =>
+    rw [vectorOf_succ]
+    ext xs
+    simp [Set.mem_setOf_eq]
+    constructor
+    . -- Forwards direction
+      intro h
+      obtain ⟨x, ⟨hmem, ⟨xs, ⟨hxs, hcons⟩⟩⟩⟩ := h
+      subst hcons
+      rw [IH] at hxs
+      dsimp at hxs
+      obtain ⟨hlen, hsupp⟩ := hxs
+      constructor
+      . simp
+        assumption
+      . -- ∀ y ∈ x :: xs, y ∈ g.support
+        intro y hy
+        rw [List.mem_cons] at hy
+        obtain ⟨heq, hy⟩ := hy
+        . assumption
+        . apply hsupp
+          assumption
+    . -- Backwards direction
+      intro ⟨hlen, hmem⟩
+      -- Since we know `hlen: xs.length = n' + 1`,
+      -- we must have `xs = y :: ys` for some `y, ys`
+      obtain ⟨y, ys, rfl⟩ := List.exists_cons_of_length_eq_add_one hlen
+      exists y
+      constructor
+      . apply hmem
+        apply List.mem_cons_self
+      . exists ys
+        constructor
+        . rw [IH]
+          apply Set.mem_sep
+          . simp at hlen
+            assumption
+          . intro x hx
+            apply hmem
+            apply List.mem_cons_of_mem
+            assumption
+        . rfl
+
+
 /-- The support of `elements xs` is exactly the set of all elements in `xs` -/
 @[simp]
 theorem support_elements
@@ -247,6 +314,7 @@ theorem mem_support_oneOf_iff
     (hne : gs ≠ []) :
     a ∈ support (oneOf gs hne) ↔ ∃ g ∈ gs, a ∈ (g ()).support := by
   simp [support_oneOf]
+
 
 
 /-- If `n < sum (fst <$> gs)`, then `frequencySelect gs n h` picks a sub-generator
